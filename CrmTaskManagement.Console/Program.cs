fb5d398d0b555b3ab8ce84fe8b60b67f8df3461c
DESKTOP-9VOSS81\Malyovanik <malyovanik.konstantin@gmail.com>
2026-07-12 18:27:25 +0300

    Initial project structure: solution, Data + Console projects, DbContext skeleton

---
+++ CrmTaskManagement.Console/Program.cs
@@ -0,0 +1,18 @@
+using CrmTaskManagement.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+IConfiguration configuration = new ConfigurationBuilder()
+    .SetBasePath(AppContext.BaseDirectory)
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: false)
+    .Build();
+
+var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+optionsBuilder.UseNpgsql(connectionString);
+
+using var db = new AppDbContext(optionsBuilder.Options);
+
+var canConnect = db.Database.CanConnect();
+Console.WriteLine($"Can connect to database: {canConnect}");
